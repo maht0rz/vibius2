0edb3728208f57f7240405dfa03304b4e6b5caa7
maht0rz <matej.sima@gmail.com>
2014-05-10 17:27:58 +0200

    Update Url.php

--- core/Url.php
+++ core/Url.php
@@ -3,7 +3,9 @@ namespace vibius\core;
 
 class Url{
     public function to($path){
-        $url = 'http://'.$_SERVER['HTTP_HOST'].explode('public/index.php',$_SERVER['PHP_SELF'])[0].$path;
+         $exp = explode('public/index.php',$_SERVER['PHP_SELF']);
+	    $exp = $exp[0]; 
+        $url = 'http://'.$_SERVER['HTTP_HOST'].$exp.$path;
         return $url;
     }
 }
