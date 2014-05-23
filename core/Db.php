7a4b8d273fabd281a43b395faefb2b08638090b5
maht0rz <matej.sima@gmail.com>
2014-05-23 18:18:18 +0200

    Update Db.php

--- core/Db.php
+++ core/Db.php
@@ -5,12 +5,14 @@ use vibius\app\settings as settings;
 class Db{
 
     public function connect(){
-        $this->pdo = new \PDO(
-            settings\config::$type . ":host=" . settings\config::$host . ";dbname=" . settings\config::$dbname . ";charset=utf8",
-            settings\config::$username,
-            settings\config::$password
-        );
-        return $this->pdo;
+        if(!isset(self::$db)){
+            self::$db = new \PDO(
+                settings\config::$type . ":host=" . settings\config::$host . ";dbname=" . settings\config::$dbname . ";charset=utf8",
+                settings\config::$username,
+                settings\config::$password
+            );
+        }
+        return self::$db;
     }
     
     public function model($name){
