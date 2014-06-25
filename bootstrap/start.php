9b83ea1351d0abac962ad2d12f3aee2f69d2d51f
maht0rz <matej.sima@gmail.com>
2014-06-25 10:28:29 +0200

    Update start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -6,6 +6,18 @@ error_reporting(E_ALL); ini_set('display_errors', '1');
 require'../vendor/autoload.php';
 require '../app/settings/handlers.php';
 require '../core/Framework.php';
-require '../core/shutdown.php';
 
+
+function myfunc($errno, $errstr, $errfile, $errline){
+   $e = new vibius\core\Error();
+   $er['file'] = $errfile;
+   $er['line'] = $errline;
+   $er['message'] = $errstr;
+   $e->handle($er);
+   die();
+   return true;
+}
+
+set_error_handler("myfunc");
 $framework->run();
+
