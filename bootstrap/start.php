868d88dff99815c552a393a5c3334eff217c16a7
maht0rz <matej.sima@gmail.com>
2014-06-25 10:34:21 +0200

    Update start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL); ini_set('display_errors', '1');
 require'../vendor/autoload.php';
 require '../app/settings/handlers.php';
 require '../core/Framework.php';
+require '../core/shutdown.php';
 
 
 function myfunc($errno, $errstr, $errfile, $errline){
