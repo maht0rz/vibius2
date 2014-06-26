49abd3c83550b48e433b84a9095f2cd46ac63f0c
dobrakmato <dobrakmato@zoho.com>
2014-06-26 07:47:05 +0200

    Update info.php
    Fixed mispelled 'memmory' to 'memory'.

--- plugins/info.php
+++ plugins/info.php
@@ -5,9 +5,7 @@ namespace vibius\plugins;
 class info{
 
     public static function memtime(){
-
-
-        return array('memmory' => (memory_get_peak_usage(true) / 1024 / 1024),'time' => (round((microtime(true) - $GLOBALS['execution_time']) * 1000, 2)));
+        return array('memory' => (memory_get_peak_usage(true) / 1024 / 1024),'time' => (round((microtime(true) - $GLOBALS['execution_time']) * 1000, 2)));
     }
 
 }
